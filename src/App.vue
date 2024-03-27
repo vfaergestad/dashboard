22980a90e904a3d4ba981ecd8125262e40e76e76
Phillip Rak <rak.phillip@gmail.com>
2024-03-27 10:26:32 -0700

    WIP: Getting a page to work with router
    Signed-off-by: Phillip Rak <rak.phillip@gmail.com>
    
    Conflicts:
    	shell/config/router.js

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref } from 'vue';
 
-const count = ref(0)
+const count = ref(0);
 
 function increment() {
-  count.value++
+  count.value++;
 }
 </script>
 
 <template>
   <div>
+    <router-view />
     <h1>NOT FAIL</h1>
     <button @click="increment">
       Click this: {{ count }}
--- vue.config.js
+++ vue.config.js
@@ -1,25 +1,34 @@
 const fs = require('fs');
 const path = require('path');
 
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
 
 const dev = (process.env.NODE_ENV !== 'production');
 const devPorts = dev || process.env.DEV_PORTS === 'true';
+const SHELL_ABS = path.join(__dirname, 'shell');
+const COMPONENTS_DIR = path.join(__dirname, 'pkg', 'rancher-components', 'src', 'components');
+
+console.log('NOT FAIL', { SHELL_ABS, COMPONENTS_DIR });
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  devServer: {
+  devServer:             {
     https: (devPorts ? {
       key:  fs.readFileSync(path.resolve(__dirname, 'shell/server/server.key')),
       cert: fs.readFileSync(path.resolve(__dirname, 'shell/server/server.crt'))
     } : null),
-    port:   (devPorts ? 8005 : 80),
-    host:   '0.0.0.0',
+    port: (devPorts ? 8005 : 80),
+    host: '0.0.0.0',
   },
   pages: {
     index: {
-      entry: path.resolve(__dirname, 'src/main.ts'),
+      entry:    path.resolve(__dirname, 'src/main.ts'),
       template: path.resolve(__dirname, 'shell/public/index.html'),
     }
+  },
+  configureWebpack(config) {
+    config.resolve.alias['~shell'] = SHELL_ABS;
+    config.resolve.alias['@shell'] = SHELL_ABS;
+    config.resolve.alias['@components'] = COMPONENTS_DIR;
   }
-})
+});
